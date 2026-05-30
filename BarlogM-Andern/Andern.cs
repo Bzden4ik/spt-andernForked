615bf6a9c9485ec382bf3919ccc059f787a5abd2
Bzden <158495797+Bzden4ik@users.noreply.github.com>
2026-05-30 20:01:21 +0200

    Update

--- BarlogM-Andern/Andern.cs
+++ BarlogM-Andern/Andern.cs
@@ -7,15 +7,15 @@ namespace BarlogM_Andern;
 
 public record ModMetadata : AbstractModMetadata
 {
-    public override string ModGuid { get; init; } = "li.barlog.andern";
+    public override string ModGuid { get; init; } = "li.bzden4ik.andern";
     public override string Name { get; init; } = "Andern";
-    public override string Author { get; init; } = "Barlog_M";
-    public override List<string>? Contributors { get; init; }
+    public override string Author { get; init; } = "Bzden4ik";
+    public override List<string>? Contributors { get; init; } = ["Barlog_M"];
     public override SemanticVersioning.Version Version { get; init; } = new("3.2.0");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
-    public override string? Url { get; init; } = "https://github.com/barlog-m/spt-andern";
+    public override string? Url { get; init; } = "https://github.com/Bzden4ik/spt-andernForked";
     public override bool? IsBundleMod { get; init; } = false;
     public override string? License { get; init; } = "MIT";
 }
